8876fce8249dc5359fca4dc681c458e98c399534
Michal Kurzelewski <mickurzelewski@gmail.com>
2022-12-18 13:43:18 +0100

    Day 18

---
+++ 2022/Day18/Program.cs
@@ -0,0 +1,74 @@
+﻿var lines = File.ReadLines(@"input.txt").ToList();
+const int MAX = 22;
+var grid = new int[MAX, MAX, MAX];
+
+int countWalls(int x, int y, int z, int color, int[,,] grid)
+{
+    var walls = 0;
+    if (!(grid[x + 1, y, z] == color)) walls++; else walls--;
+    if (!(grid[x - 1, y, z] == color)) walls++; else walls--;
+    if (!(grid[x, y + 1, z] == color)) walls++; else walls--;
+    if (!(grid[x, y - 1, z] == color)) walls++; else walls--;
+    if (!(grid[x, y, z + 1] == color)) walls++; else walls--;
+    if (!(grid[x, y, z - 1] == color)) walls++; else walls--;
+    return walls;
+}
+
+var sum = 0;
+foreach (var line in lines)
+{
+    var c = line.Split(',').Select(int.Parse).Select(x => x + 1).ToArray();
+    sum += countWalls(c[0], c[1], c[2], 1, grid);
+    grid[c[0], c[1], c[2]] = 1;
+}
+Console.WriteLine(sum);
+
+void ColorPocket(int x, int y, int z, int color)
+{
+    if (x < 0 || x > MAX - 1 ||
+        y < 0 || y > MAX - 1 ||
+        z < 0 || z > MAX - 1) return;
+    if (grid[x, y, z] == 0)
+    {
+        grid[x, y, z] = color;
+        ColorPocket(x + 1, y, z, color);
+        ColorPocket(x - 1, y, z, color);
+        ColorPocket(x, y + 1, z, color);
+        ColorPocket(x, y - 1, z, color);
+        ColorPocket(x, y, z + 1, color);
+        ColorPocket(x, y, z - 1, color);
+    }
+    if (grid[x, y, z] == 1) return;
+}
+
+var color = 2;
+for (int x = 0; x < MAX; x++)
+    for (int y = 0; y < MAX; y++)
+        for (int z = 0; z < MAX; z++)
+            if (grid[x, y, z] == 0)
+                ColorPocket(x, y, z, color++);
+
+var pocketSums = Enumerable.Range(3, color).ToDictionary(x => x, y => 0);
+var colorsToIgnore = new int[]{ 0, 1, 2 };
+for (int x = 0; x < MAX; x++)
+{
+    for (int y = 0; y < MAX; y++)
+    {
+        for (int z = 0; z < MAX; z++)
+        {
+            color = grid[x, y, z];
+            if (colorsToIgnore.Contains(color)) 
+                continue;
+            else
+            {
+                pocketSums[color] += countWalls(x, y, z, color, grid);
+                grid[x, y, z] = 1;
+            }
+        }
+    }
+}
+
+foreach (var pocketSum in pocketSums.Values)
+    sum -= pocketSum;
+
+Console.WriteLine(sum);
--- README.md
+++ README.md
@@ -21,3 +21,4 @@ My solutions to the Advent of Code - https://adventofcode.com
 | [Day 14: Regolith Reservoir](https://adventofcode.com/2022/day/14) | [Source](2022/Day14/Program.cs) |
 | [Day 15: Beacon Exclusion Zone](https://adventofcode.com/2022/day/15) | [Source](2022/Day15/Program.cs) |
 | [Day 16: Proboscidea Volcanium](https://adventofcode.com/2022/day/16) | [Source](2022/Day16/Program.cs) |
+| [Day 18: Boiling Boulders](https://adventofcode.com/2022/day/18) | [Source](2022/Day18/Program.cs) |
