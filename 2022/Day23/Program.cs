eef7858d4459691298f99dba4264a24e35ca8279
Michal Kurzelewski <mickurzelewski@gmail.com>
2022-12-27 15:37:40 +0100

    Day 23

--- 2022/Day23/Program.cs
+++ 2022/Day23/Program.cs
@@ -14,27 +14,81 @@ for (int i = 0; i < lines.Count; i++)
     }
 }
 
-foreach (var elf in elfs)
+var plannedMoves = new Dictionary<string, Elf>();
+
+for (var round = 0; round < 1000; round++)
 {
-    var NW = board.Get(elf.X[0] - 1, elf.Y[0] - 1);
-    var N = board.Get(elf.X[0], elf.Y[0] - 1);
-    var NE = board.Get(elf.X[0] + 1, elf.Y[0] - 1);
-    var E = board.Get(elf.X[0] + 1, elf.Y[0]);
-    var SE = board.Get(elf.X[0] + 1, elf.Y[0] + 1);
-    var S = board.Get(elf.X[0], elf.Y[0]) + 1;
-    var SW = board.Get(elf.X[0] - 1, elf.Y[0] + 1);
-    var W = board.Get(elf.X[0] - 1, elf.Y[0]);
+    plannedMoves.Clear();
+    foreach (var elf in elfs)
+    {
+        var NW = board.Get(elf.X[0] - 1, elf.Y[0] - 1);
+        var N = board.Get(elf.X[0], elf.Y[0] - 1);
+        var NE = board.Get(elf.X[0] + 1, elf.Y[0] - 1);
+        var E = board.Get(elf.X[0] + 1, elf.Y[0]);
+        var SE = board.Get(elf.X[0] + 1, elf.Y[0] + 1);
+        var S = board.Get(elf.X[0], elf.Y[0] + 1);
+        var SW = board.Get(elf.X[0] - 1, elf.Y[0] + 1);
+        var W = board.Get(elf.X[0] - 1, elf.Y[0]);
 
+        var directions = new[]
+        {
+            new {available = new[] {NW, N, NE}.All(x => x == default), next = new[] {elf.X[0], elf.Y[0] - 1}},
+            new {available = new[] {SE, S, SW}.All(x => x == default), next = new[] {elf.X[0], elf.Y[0] + 1}},
+            new {available = new[] {SW, W, NW}.All(x => x == default), next = new[] {elf.X[0] - 1, elf.Y[0]}},
+            new {available = new[] {NE, E, SE}.All(x => x == default), next = new[] {elf.X[0] + 1, elf.Y[0]}}
+        };
 
+        var all = directions.All(x => x.available);
+        var none = directions.All(x => !x.available);
 
+        if (all || none) continue;
+
+        for (var i = 0; i < 4; i++)
+        {
+            if (directions[(i + round) % 4].available)
+            {
+                var next = directions[(i + round) % 4].next;
+                if (plannedMoves.TryGetValue($"{next[0]} {next[1]}", out var youAreNotGoing))
+                    youAreNotGoing.Clear();
+                else
+                {
+                    elf.SetNext(next);
+                    plannedMoves[$"{next[0]} {next[1]}"] = elf;
+                }
+                break;
+            }
+        }
+    }
+
+    if (plannedMoves.Count == 0)
+    {
+        Console.WriteLine($"No elfs moved on round {round+1}");
+        break;
+    }
+
+    foreach (var elf in elfs)
+    {
+        board.Move(elf.X[0], elf.Y[0], elf.X[1], elf.Y[1]);
+        elf.Move();
+    }
+
+    if (round == 9)
+    {
+        var minX = elfs.Min(elf => elf.X[0]);
+        var maxX = elfs.Max(elf => elf.X[0]);
+        var maxY = elfs.Max(elf => elf.Y[0]);
+        var minY = elfs.Min(elf => elf.Y[0]);
+
+        Console.WriteLine((Math.Abs(minX - maxX) + 1) * (Math.Abs(minY - maxY) + 1) - elfs.Count);
+    }
 }
-
 
 
 class Board
 {
     private const int offset = 1000;
-    public char[,] _board { get; set; } = new char[5000, 5000];
+
+    public char[,] _board { get; set; } = new char[3000, 3000];
 
     public void Mark(int x, int y) => _board[x + offset, y + offset] = '#';
 
@@ -49,12 +103,30 @@ class Board
 
 class Elf
 {
-    public int[] X { get; set; } = new int[2];
-    public int[] Y { get; set; } = new int[2];
+    public int[] X { get; set; }
+    public int[] Y { get; set; }
+
+    public void SetNext(int[] next)
+    {
+        X[1] = next[0];
+        Y[1] = next[1];
+    }
 
     public Elf(int x, int y)
     {
-        X[0] = x;
-        Y[0] = y;
+        X = new[] {x, x};
+        Y = new[] {y, y};
+    }
+
+    public void Clear()
+    {
+        X[1] = X[0];
+        Y[1] = Y[0];
+    }
+
+    public void Move()
+    {
+        X[0] = X[1];
+        Y[0] = Y[1];
     }
 }
