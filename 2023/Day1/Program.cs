29003c76f629023f00baad43f3104234bb07c28b
Michal Kurzelewski <mickurzelewski@gmail.com>
2023-12-01 16:26:18 +0100

    and some more powder to day1

--- 2023/Day1/Program.cs
+++ 2023/Day1/Program.cs
@@ -1,4 +1,4 @@
-﻿var task1 = File.ReadLines(@"input.txt").ToList();
+﻿var task1 = File.ReadLines(@"input.txt");
 var task2 = task1.Select(x => x.Replace("one", "one1one")
     .Replace("two", "t2wo")
     .Replace("three", "th3ree")
@@ -7,12 +7,11 @@ var task2 = task1.Select(x => x.Replace("one", "one1one")
     .Replace("six", "si6x")
     .Replace("seven", "se7ven")
     .Replace("eight", "ei8ght")
-    .Replace("nine", "ni9ne"))
-    .ToList();
+    .Replace("nine", "ni9ne"));
 
-static int Sum(List<string> lines) => lines
-    .Select(x => x.Where(char.IsDigit).ToArray())
-    .Select(x => int.Parse([x[0], x[^1]]))
+static int Sum(IEnumerable<string> lines) => lines
+    .Select(x => x.Where(char.IsDigit))
+    .Select(x => int.Parse([x.First(), x.Last()]))
     .Sum();
 
 Console.WriteLine(Sum(task1));
