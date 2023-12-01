0085f91e937277c408e18902ac1344becf0c50d5
Michal Kurzelewski <mickurzelewski@gmail.com>
2023-12-01 10:59:55 +0100

    day1 of 2023

---
+++ 2023/Day1/Program.cs
@@ -0,0 +1,19 @@
+﻿var task1 = File.ReadLines(@"input.txt").ToList();
+var task2 = task1.Select(x => x.Replace("one", "one1one")
+    .Replace("two", "two2two")
+    .Replace("three", "three3three")
+    .Replace("four", "four4four")
+    .Replace("five", "five5five")
+    .Replace("six", "six6six")
+    .Replace("seven", "seven7seven")
+    .Replace("eight", "eight8eight")
+    .Replace("nine", "nine9nine"))
+    .ToList();
+
+static int Sum(List<string> lines) => lines
+    .Select(line => line.Where(char.IsDigit).ToArray())
+    .Select(line => int.Parse($"{line[0]}{line[^1]}"))
+    .Sum();
+
+Console.WriteLine(Sum(task1));
+Console.WriteLine(Sum(task2));
