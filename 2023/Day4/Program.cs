4a9e92b2554f317e7c22b9f48c9f22af080bb31a
Michal Kurzelewski <mickurzelewski@gmail.com>
2023-12-04 09:00:52 +0100

    day4

---
+++ 2023/Day4/Program.cs
@@ -0,0 +1,18 @@
+﻿var lines = File.ReadLines(@"input.txt").Select(x => x.Replace("  ", " ")).ToList();
+
+var sum = 0;
+var cards = Enumerable.Repeat(1, lines.Count).ToArray();
+for (int i = 0; i < lines.Count; i++)
+{
+    var left = lines[i].Split(": ")[1].Split(" | ")[0].Split(" ").Select(int.Parse);
+    var right = lines[i].Split(": ")[1].Split(" | ")[1].Split(" ").Select(int.Parse).ToHashSet();
+
+    var score = left.Where(right.Contains).Count();
+    sum += (int)Math.Pow(2, score - 1);
+
+    for (int j = 1; j <= score; j++)
+        cards[i + j] += cards[i];
+}
+
+Console.WriteLine(sum);
+Console.WriteLine(cards.Sum());
